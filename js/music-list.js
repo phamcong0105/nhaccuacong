a9ad53a92d835d4f37dfddcad4d7b58a56cd5a12
phamcongbfl <81978052+phamcong0105@users.noreply.github.com>
2021-12-09 14:45:23 +0700

    add mywebsite file

---
+++ js/music-list.js
@@ -0,0 +1,58 @@
+let allMusic = [
+  {
+    name: "Trời Giấu Trời Mang Đi",
+    artist: "Amee x ViruSs",
+    img: "music-4",
+    src: "music-1"
+  },
+  {
+    name: "Giấc Mơ Rất Thơ",
+    artist: "Mer",
+    img: "music-4",
+    src: "music-2"
+  },
+  {
+    name: "Tình Thật Tình Mơ",
+    artist: "Mer",
+    img: "music-4",
+    src: "music-3"
+  },
+  {
+    name: "Dịu Dàng Em Đến",
+    artist: "ERIK",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Em Hát Ai Nghe (Lofi Ver.)",
+    artist: "Orange x Freak D",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+  name: "Có Hẹn Với Thanh Xuân",
+  artist: "MONSTAR",
+  img: "music-6",
+  src: "music-6"
+},
+{
+  name: "Nằm Ru Em Ngủ",
+  artist: "Bích Phương",
+  img: "music-7",
+  src: "music-7"
+},
+{
+  name: "Chiếc Hộp",
+  artist: "Bích Phương",
+  img: "music-8",
+  src: "music-8"
+},
+
+
+
+
+
+
+
+
+];
